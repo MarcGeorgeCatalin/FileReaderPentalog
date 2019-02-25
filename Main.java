4608bb283cc38a90ccddc63915c12d141ca208d7
george <marcgeorgecatalin@gmail.com>
2019-02-25 16:18:38 +0200

    v4

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import fileReader.ReaderConsoleOperation;
+import fileReader.ConsolePrinter;
 import fileReader.FileReader;
 
 public class Main {
     public static void main(String[] args) {
         String filePath = "C:\\Users\\George\\Desktop\\pentalog\\fi.txt";
 
-        ReaderConsoleOperation.print(FileReader.getFileReader(filePath));
+        ConsolePrinter.print(FileReader.getFileReader(filePath));
     }
 }
--- fileReader/ConsolePrinter.java
+++ fileReader/ConsolePrinter.java
@@ -2,7 +2,7 @@ package fileReader;
 
 import fileReader.api.Reader;
 
-public class ReaderConsoleOperation {
+public class ConsolePrinter {
     static public void print(Reader reader){
         reader.getContent().forEach(System.out::println);
     }
