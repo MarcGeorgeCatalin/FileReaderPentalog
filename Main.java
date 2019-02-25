3c3e52f8569fdb4c3598d035541f23fbc5f1ff34
george <marcgeorgecatalin@gmail.com>
2019-02-25 14:15:07 +0200

    v4

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         String filePath = "C:\\Users\\George\\Desktop\\pentalog\\fi.txt";
 
-        ReaderConsoleOperation.printToConsole(FileReader.getFileReader(filePath));
+        ReaderConsoleOperation.print(FileReader.getFileReader(filePath));
     }
 }
